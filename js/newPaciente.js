ac96e851e90e12d587fc7286d3b4401cab4137d6
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2021-01-25 00:57:45 -0300

    added object after saved to table

--- js/newPaciente.js
+++ js/newPaciente.js
@@ -9,8 +9,28 @@ function save(event){
     
     var nome = form.nome.value;
     var peso = form.peso.value;
-    var altura = form.peso.altura;
-    var gordura = form.peso.gordura;
+    var altura = form.altura.value;
+    var gordura = form.gordura.valua;
 
+    var pacienteTr = document.createElement("tr");
     
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = 20;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
 }
